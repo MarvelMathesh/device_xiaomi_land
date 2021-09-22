3fa24868d346fe581e9c542ee8e7840f36ec3367
MarvelMathesh <123v.mathesh@gmail.com>
2021-09-22 23:41:01 +0530

    land: Switch to GCamGo Gcam Go Mod: CameraGo_2.5_Greatness_Special-2-AUX

--- device.mk
+++ device.mk
@@ -120,16 +120,34 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.3:64 \
+    android.hardware.camera.device@3.4:64 \
+    android.hardware.camera.device@3.5:64 \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    camera.msm8937 \
+    android.hardware.camera.provider@2.5:64 \
+    android.hardware.camera.provider@2.6:64 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    libdng_sdk.vendor \
+    libstdc++.vendor \
+    libgui_vendor \
+    libcamshim \
     libmm-qcamera \
-    GCam
+    libui_shim
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.4 \
-    android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0
+    camera.msm8937 \
+    GCamGo
+
+# b/166675194
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.camera.provider24.disable_mem_init=1
 
 # Component overrides
 PRODUCT_COPY_FILES += \
