542821fd86ef4e1626152626b74ab25790dd6a79
Mikhail Naganov <mnaganov@google.com>
2021-11-07 12:46:49 +0400

    land: switch audio HAL to V7 Updated audio policy manager configuration files to comply with the changes in the XML schema.
    Bug: 142480271
    Test: atest VtsHalAudioV7_0TargetTest
    Test: atest VtsHalAudioEffectV7_0TargetTest
    Change-Id: I55d3728c809d27e58d5d496a1c7d637050dde53a
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -105,14 +105,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.audio@6.0 \
-    android.hardware.audio@6.0-impl:32 \
-    android.hardware.audio.common@6.0 \
-    android.hardware.audio.common@6.0-util \
+    android.hardware.audio@7.0 \
+    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio.common@7.0 \
+    android.hardware.audio.common@7.0-util \
     android.hardware.audio.effect@2.0-impl:32 \
-    android.hardware.audio.effect@6.0 \
-    android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.audio.effect@7.0 \
+    android.hardware.audio.effect@7.0-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
 
 # Audio features
 PRODUCT_PACKAGES += \
@@ -134,7 +134,7 @@ PRODUCT_COPY_FILES += \
 # XML Audio configuration files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
