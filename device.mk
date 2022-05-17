d50a9bafd787f723f1b23bb0e1c4c9d516d80ef9
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-05-17 14:12:42 +0400

    land: disable compressed APEX Compressed APEX has been introduced in Android 12, and they are enabled by default inheriting updatable_apex.mk. Unfortunately, when the device is encrypted, they are causing bootloops on msm8937. As the only advantage is some space gain on disk, we can simply disable them: keep the variable immediatly after including the updatable_apex.mk to avoid being overwritten.
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>
    Change-Id: I410bd4ad8d589971339dddab709c5b40d9c9a15c
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -82,6 +82,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Apex
+PRODUCT_COMPRESSED_APEX := false
+
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
